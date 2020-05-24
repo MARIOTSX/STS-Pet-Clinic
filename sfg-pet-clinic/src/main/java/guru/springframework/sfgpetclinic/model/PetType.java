2f9a581aac2b976f6739487be7c8730eec0a45b8
Mariotsx <zacek3082@gmail.com>
2020-05-24 17:24:48 -0500

    POJOS .Closes #2

---
+++ sfg-pet-clinic/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+    private String getName;
+
+    public String getGetName() {
+        return getName;
+    }
+
+    public void setGetName(String getName) {
+        this.getName = getName;
+    }
+
+    
+}
