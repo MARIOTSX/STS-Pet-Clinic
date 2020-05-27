be1a8adcebe52a6d9f63483fac97d69fc511de69
Mariotsx <zacek3082@gmail.com>
2020-05-27 18:49:39 -0500

    Closes #10

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -4,10 +4,8 @@ import java.util.Set;
 
 import guru.springframework.sfgpetclinic.model.*;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner,Long> {
 
    Owner findByLastName(String LastName);
-   Owner findById(Long id);
-   Owner save(Owner owner);
-   Set<Owner> findAll();   
+   
 }
